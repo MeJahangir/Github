87bb563eac10dc642ece73bc028f699ba437dde9
MD JAHANGIR ALAM <you@example.com>
2024-11-16 11:37:48 +0600

    submit code

--- github-testing/ff.js
+++ github-testing/ff.js
@@ -1,4 +1,17 @@
 
+
+for (let index = 1; index <= 100; index++) {
+    const element = index;
+    console.log(element);
+}
+
+
+for (let index = 1; index <= 100; index++) {
+    const element = index;
+    console.log(element);
+}
+
+
 for (let index = 1; index <= 100; index++) {
     const element = index;
     console.log(element);
